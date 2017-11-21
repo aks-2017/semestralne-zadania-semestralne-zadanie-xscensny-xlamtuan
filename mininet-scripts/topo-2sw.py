f8b2a9c613f5982ff86f89ce59a7b96599f2cb92
None <None>
2017-11-21 05:11:01 -0800

    added create topology

---
+++ mininet-scripts/topo-2sw.py
@@ -0,0 +1,22 @@
+from mininet.topo import Topo
+
+class MyTopo( Topo ):
+	def __init__(self):
+	#def build(self):
+	# Create simple topo
+		Topo.__init__( self )
+
+	# add hosts
+		switch1 = self.addSwitch( 's1' )
+		switch2 = self.addSwitch( 's2' )
+		switch3 = self.addSwitch( 's3' )
+		switch4 = self.addSwitch( 's4' )	
+		# add links
+		self.addLink( switch1, switch2)
+		self.addLink( switch1, switch3)
+		self.addLink( switch1, switch4)
+		self.addLink(switch2, switch3)
+		self.addLink(switch2, switch4)
+
+topos = { 'mytopo': MyTopo }
+	
