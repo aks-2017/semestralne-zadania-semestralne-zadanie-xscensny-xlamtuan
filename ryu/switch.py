b74a6ece8352a20bdaf9f0a5117fbcf1f34ee108
KnucklePunch <patrik.scensny@gmail.com>
2017-11-27 12:24:56 +0100

    modified switch

--- ryu/switch.py
+++ ryu/switch.py
@@ -20,7 +20,7 @@ An OpenFlow 1.0 L2 learning switch implementation.
 
 from ryu.base import app_manager
 from ryu.controller import ofp_event
-from ryu.controller.handler import MAIN_DISPATCHER
+from ryu.controller.handler import MAIN_DISPATCHER, CONFIG_DISPATCHER
 from ryu.controller.handler import set_ev_cls
 from ryu.ofproto import ofproto_v1_0
 from ryu.lib.mac import haddr_to_bin
